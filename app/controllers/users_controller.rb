86da31b879888ef3996178cf69749273b141a140
yuyahs <yuya.tennnis216@icloud.com>
2021-08-12 12:12:05 +0900

    finish login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
+      log_in @user
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'static_pages/home'
   get '/about', to: 'static_pages#about'
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   get '/policy', to: 'static_pages#policy'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
 
