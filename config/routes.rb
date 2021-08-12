a03438ecdcc9579c848f4d9c248ce6672fb15163
yuyahs <yuya.tennnis216@icloud.com>
2021-08-12 10:58:53 +0900

    finish signup function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/policy', to: 'static_pages#policy'
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :users
+
 
 end
