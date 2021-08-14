5d5122bb22850c31a6cebd4d9168cb16d307a180
yuyahs <yuya.tennnis216@icloud.com>
2021-08-14 21:04:26 +0900

    add user edit and update

--- spec/requests/users_requests_spec.rb
+++ spec/requests/users_requests_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do
+    @user = create(:user)
+    @other_user = create(:user)
+  end
   describe "post/ signup_path " do
     it "as an invalid user" do
       post users_path, params: { user: { name:  "",
@@ -23,39 +27,119 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "post/ login_path" do
-    before do
-      @user = create(:user)
+    it "as an invalid user" do
+      post login_path, params: { session: { email: "invalid@example.com",
+      password: 'password' } }
+      expect(response).to_not redirect_to user_path(@user)
     end
-      it "as an invalid user" do
-        post login_path, params: { session: { email: "invalid@example.com",
-        password: 'password' } }
-        expect(response).to_not redirect_to user_path(@user)
-      end
 
-      it "as a valid user" do
-        post login_path, params: { session: { email: @user.email,
-        password: 'password' } }
-        expect(response).to redirect_to user_path(@user)
-      end
+    it "as a valid user" do
+      post login_path, params: { session: { email: @user.email,
+      password: 'password' } }
+      expect(response).to redirect_to user_path(@user)
+    end
 
-      it "remember me" do
-        log_in_as @user
-        expect(cookies[:remember_token]).not_to eq nil
-      end
+    it "remember me" do
+      log_in_as @user
+      expect(cookies[:remember_token]).not_to eq nil
+    end
 
-      it "does not remembers the cookie when user does not checks the Remember Me box" do
-        log_in_as(@user, remember_me: '0')
-        expect(cookies[:remember_token]).to eq nil
-      end
+    it "does not remembers the cookie when user does not checks the Remember Me box" do
+      log_in_as(@user, remember_me: '0')
+      expect(cookies[:remember_token]).to eq nil
+    end
   end
   describe "delete/ logout_path" do
     it "redirects to root_path" do
-      @user = create(:user)
       post login_path ,params: { session: { email: @user.email,
         password: 'password' } }
       delete logout_path
       expect(response).to redirect_to root_path
       expect(is_logged_in?).to be_falsy
+    end
+  end
+
+  describe "get/ edit_user_path" do
+    context "ログイン済みユーザーのアクセス" do
+      it "redirects to edit_user_path" do
+        log_in_as @user
+        get edit_user_path(@user)
+        expect(response.body).to include "登録情報を変更する"
+      end
+    end
+
+    context "未ログインユーザーのアクセス" do
+      it "returns to login_path" do
+        get edit_user_path(@user)
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    context "間違ったユーザーのアクセス" do
+      it "redirects to root_path" do
+        log_in_as @user
+        get edit_user_path(@other_user)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "patch/ edit_user_path" do
+    context "ログイン済みユーザーのアクセス" do
+      before do
+        log_in_as @user
+      end
+      it "accepts valid information even if password is blank" do
+        patch user_path(@user), params: {user: {
+            name: "Foo Bar",
+            email: "foo@bar.com",
+            password: "",
+            password_confirmation: ""
+        } }
+        expect(response).to redirect_to user_path(@user)
+      end
+
+      it "rejects invalid information" do
+        patch user_path(@user), params: {user: {
+            name: "",
+            emai: "foo@invalid",
+            password: "foo",
+            password_confirmation: "bar"
+        } }
+        expect(response.body).to include "登録情報を変更する"
+      end
+    end
+    context "未ログインユーザーのアクセス" do
+      it "returns to login_path" do
+        patch user_path(@user), params: {user: {
+          name: "Foo Bar",
+          email: "foo@bar.com",
+          password: "",
+          password_confirmation: ""
+      } }
+      expect(response).to redirect_to login_path
+      end
+    end
+
+    context "間違ったユーザーのアクセス" do
+      it "redirects to root_path" do
+        log_in_as @user
+        patch user_path(@other_user), params: {user: {
+          name: "Foo Bar",
+          email: "foo@bar.com",
+          password: "",
+          password_confirmation: ""
+        } }
+      expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "フレンドリーフォワーディング" do
+    it "redirects to edit_user_path" do
+      get edit_user_path(@user)
+      log_in_as @user
+      expect(response).to redirect_to edit_user_path(@user)
     end
   end
 end
