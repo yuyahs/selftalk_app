2e99e5b77cc4a64677298c46273192871072286b
yuyahs <yuya.tennnis216@icloud.com>
2021-08-17 08:02:34 +0900

    add development  setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
