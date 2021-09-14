c31c2405d20105617a5ee2050ce156289181a246
yuyahs <yuya.tennnis216@icloud.com>
2021-09-14 21:02:47 +0900

    add-guest-login-rspec

---
+++ spec/system/static_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :system do
+
+  describe "Guest login" do
+
+    it "shows root page but doesn't show menus" do
+      visit root_path
+      click_button "ゲストログイン"
+      expect(page).to_not have_content "マイページ"
+    end
+  end
+
+end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,12 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :system do
   let(:user){create(:user)}
 
-
   describe 'User page' do
     let!(:question){create(:question, :mode_2)}
     let!(:answer){create(:answer)}
-
-
 
     it "shows answer records" do
       log_in user
@@ -17,9 +14,8 @@ RSpec.describe User, type: :system do
       click_button '次の問題へ進む'
       visit user_path(user)
       expect(page).to have_content "#{answer.created_at.to_date}"
-
     end
-
-
   end
+
+ 
 end
