e598eb2fa36377357b54dd09f67255180dff48fe
yuyahs <yuya.tennnis216@icloud.com>
2021-10-01 13:49:14 +0900

    create basic footer with vue

--- app/javascript/src/App.vue
+++ app/javascript/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <Header nav1="ログイン" nav2="新規登録"/>
+  <div>
+    <Header nav1="ログイン" nav2="新規登録"/>
+
+    <Footer link1="home" link2="about" link3="contact" link4="利用規約"/>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
