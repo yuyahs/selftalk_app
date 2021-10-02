4650eb557e622ca754b8ca1354eebec0b00e6f62
yuyahs <yuya.tennnis216@icloud.com>
2021-10-02 09:17:29 +0900

    feat: signup function

--- app/javascript/src/components/Header.vue
+++ app/javascript/src/components/Header.vue
@@ -2,10 +2,11 @@
   <header class="bg-blue-900 text-white h-16 border-b-2 border-fuchsia-600">
     <router-link to="/" class="font-serif text-3xl ">SelftalkEnglish</router-link>
     <nav class="float-right flex flex-row text-white font-bold mt-4 mr-10 ">
-      <ul class="flex flex-row ">
-        <li class="mr-4 hover:bg-blue-300 ">{{nav1}}</li>
-        <li class="mr-4 hover:bg-blue-300">{{nav2}}</li>
-      </ul>
+      <div class="flex flex-row ">
+        <p class="mr-4 hover:bg-blue-300">{{nav1}}</p>
+        <router-link to="/users/new" class="mr-4 hover:bg-blue-300 ">{{nav2}}
+        </router-link>
+      </div>
 
     </nav>
   </header>
--- spec/requests/api/users_request_spec.rb
+++ spec/requests/api/users_request_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Api::Users", type: :request do
+# RSpec.describe "Api::Users", type: :request do
 
-end
+# end
