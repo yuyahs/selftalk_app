b23ba959a0c29b1041ec2b79efb4ad067f949285
yuyahs <yuya.tennnis216@icloud.com>
2021-10-09 09:01:44 +0900

    doc: add basic codes to courses component

--- app/javascript/src/password_resets/Edit.vue
+++ app/javascript/src/password_resets/Edit.vue
@@ -1,19 +1,19 @@
 <template>
-<div class="flex flex-col w-full max-w-md h-full container mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-  <h1 class="text-center font-bold text-2xl mb-6">新しいパスワードを設定する</h1>
-  <input type="hidden" v-model="email">
+  <form class="flex flex-col w-full max-w-md h-full container mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+    <h1 class="text-center font-bold text-2xl mb-6">新しいパスワードを設定する</h1>
+    <input type="hidden" v-model="email">
 
-  <label for="user_password">新しいパスワード</label>
-  <input type="password" v-model="user.password" class="pl-3 h-10 w-full border-solid border-2 rounded border-gray-600">
+    <label for="user_password">新しいパスワード</label>
+    <input type="password" v-model="user.password" class="pl-3 h-10 w-full border-solid border-2 rounded border-gray-600">
 
-  <label for="user_password_confirmation">パスワード再入力</label>
-  <input type="password" v-model="user.password_confirmation" class="pl-3 h-10 w-full border-solid border-2 rounded border-gray-600">
+    <label for="user_password_confirmation">パスワード再入力</label>
+    <input type="password" v-model="user.password_confirmation" class="pl-3 h-10 w-full border-solid border-2 rounded border-gray-600">
 
-  <button @click="setPassword" class="mt-4 w-2/5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-    再設定
-  </button>
+    <button @click="setPassword" class="mt-4 w-2/5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      再設定
+    </button>
 
-</div>
+ </form>
 
 </template>
 
@@ -52,6 +52,7 @@
         })
       }
     }
+
 
   }
 
