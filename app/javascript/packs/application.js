40278a1026dd13fdbd049389c64b085a20b21775
yuyahs <yuya.tennnis216@icloud.com>
2021-10-24 22:46:11 +0900

    feat: add timer method

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,12 +14,15 @@ import App from "../src/App.vue";
 import { router } from '../src/router';
 import FlashMessage from '@smartweb/vue-flash-message';
 import { store } from '../src/store.js';
+import SetInterval from '../src/plugins/SetInterval';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = createApp(App);
   app.use(router);
   app.use(store);
   app.use(FlashMessage);
+  app.use(SetInterval);
   app.mount("#application");
 });
 
--- app/javascript/src/App.vue
+++ app/javascript/src/App.vue
@@ -10,17 +10,28 @@
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
+  import Header from './components/Header'
+  import Footer from './components/Footer'
 
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
+
+
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Footer,
+    },
+    // created() {
+    //    if(document.body.contains(document.getElementById('progress'))) {
+    //      this.$clearAllInterval();
+    //    }
+    // }
+
+
+
   }
-}
 </script>
 
 
