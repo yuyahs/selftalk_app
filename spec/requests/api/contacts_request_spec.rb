303b12173b514b8ff62e688d3382968fe8f87b2c
yuyahs <yuya.tennnis216@icloud.com>
2021-11-05 19:59:28 +0900

    test: api contacts controller

--- spec/requests/api/contacts_request_spec.rb
+++ spec/requests/api/contacts_request_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Contacts", type: :request do
+  describe "GET/new" do
+    it "成功レスポンス204を返す" do
+      get "/api/contacts/new"
+      expect(response).to have_http_status "204"
+    end
+  end
 
+  describe "POST/create" do
+    context "名前が空欄でメッセージは入力されている場合" do
+      it "エラーレスポンス400を返す" do
+        expect{
+          post "/api/contacts", params: {inquiry: {name: "", message: "message"}}
+        }.to change(Inquiry, :count).by(0)
+        expect(ActionMailer::Base.deliveries.size).to eq(0)
+        expect(response).to have_http_status "400"
+      end
+    end
+    context "名前が入力されていてでメッセージは空の場合" do
+      it "エラーレスポンス400を返す" do
+        expect{
+          post "/api/contacts", params: {inquiry: {name: "user", message: ""}}
+        }.to change(Inquiry, :count).by(0)
+        expect(ActionMailer::Base.deliveries.size).to eq(0)
+        expect(response).to have_http_status "400"
+      end
+    end
+    context "名前もメッセージも入力されている場合" do
+      it "成功レスポンス200を返す" do
+        expect{
+          post "/api/contacts", params: {inquiry: {name: "user", message: "message"}}
+        }.to change(Inquiry, :count).by(1)
+        expect(ActionMailer::Base.deliveries.size).to eq(1)
+        expect(response).to have_http_status "204"
+      end
+    end
+  end
 end
--- spec/requests/contacts_request_spec.rb
+++ spec/requests/contacts_request_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
+  
 
 end
