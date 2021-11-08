92d30caf77a8bb9ba8abc7aeddc2468dd8641f19
yuyahs <yuya.tennnis216@icloud.com>
2021-11-08 19:57:04 +0900

    fix: db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
+User.create(
+  id: 1,
+  name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
@@ -8,18 +10,27 @@ User.create!(name:  "Example User",
   activated_at: Time.zone.now,
   admin: true)
 
-Question.create!(content: "test question",
+Question.create(
+ id: 1,
+ content: "test question",
  mode_num: 1)
-Question.create!(content: "test question2",
+Question.create(
+ id: 2,
+ content: "test question2",
  mode_num: 1)
-Question.create!(content: "test statement",
+Question.create(
+ id: 3,
+ content: "test statement",
  mode_num: 2)
-Question.create!(content: "test statement2",
+Question.create(
+ id: 4,
+ content: "test statement2",
  mode_num: 2)
 
 
 
-Answer.create!(content: "test",
+Answer.create(
+  content: "test",
   user_id: 1,
   question_id: 1,
   created_at: Time.current.yesterday)
