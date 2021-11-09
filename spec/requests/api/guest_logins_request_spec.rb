deb41f8e3aaad3a76ad4280af3f7e1d7eb708931
yuyahs <yuya.tennnis216@icloud.com>
2021-11-09 20:20:09 +0900

    test: api/guest_login

--- spec/requests/api/guest_logins_request_spec.rb
+++ spec/requests/api/guest_logins_request_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Api::GuestLogins", type: :request do
 
+  describe "GET/ guest_sign_in" do
+
+    it "成功レスポンス200を返す" do
+      post "/api/guest_sign_in"
+      expect(response).to have_http_status "200"
+    end
+
+    context "guest ログイン中にuser&answer controllerのアクションを実行しようとした場合" do
+      let(:user){create(:user)}
+      before do
+        post "/api/guest_sign_in"
+      end
+
+      it "redirects to root url" do
+        get api_user_path(user)
+        expect(response).to redirect_to root_path
+      end
+
+      it "redirects to root url" do
+        get "/api/answers"
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
 end
