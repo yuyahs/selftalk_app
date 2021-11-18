6488ccb94a0fea78192a4eca7b55d32391b38338
yuyahs <yuya.tennnis216@icloud.com>
2021-11-19 08:31:09 +0900

    test: item model rspec

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
-  validates :content, presence: true, length: {maximum: 20}
-  validates :meaning, presence: true, length: {maximum: 20}
+  validates :content, presence: true, length: {maximum: 50}
+  validates :meaning, presence: true, length: {maximum: 50}
   validates :memo, presence: true, length: {maximum: 100}
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
 end
