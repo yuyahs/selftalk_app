30030896c5399b14bceec9188f2511dec4291a91
yuyahs <yuya.tennnis216@icloud.com>
2021-11-21 15:37:09 +0900

    fix: transfer some tailwind to custom.scss

--- app/javascript/src/pages/Home.vue
+++ app/javascript/src/pages/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="text-center">
     <div v-if="$store.state.loggedIn">
-      <h1 class="text-center text-white text-3xl">学習コース一覧</h1>
-      <div class ="bg-blue-300 p-4 mt-8 mx-auto md:w-1/2 text-center text-black text-3xl
-                      rounded-lg border-solid border-4 border-white shadow-outline">
+      <h1 class="text-white text-3xl">学習コース一覧</h1>
+      <div class ="course-layout">
         <h1 class = "font-bold mb-4">Self Explain</h1>
         <img class="mx-auto w-1/2 h-24" src="/assets/explain.svg">
         <p class="text-lg mt-4">質問に対して説明形式で回答</p>
@@ -13,8 +12,7 @@
         </button>
       </div>
 
-      <div class ="bg-blue-300 container mx-auto md:w-1/2 my-20 py-3.5 text-center text-black text-3xl
-                      rounded-lg border-solid border-4 border-white shadow-outline">
+      <div class ="course-layout">
         <h2 class = "font-bold mb-4">Self Reaction</h2>
         <img class="mx-auto w-1/2 h-24" src="/assets/reaction.svg">
         <p class="text-lg mt-4">発言に対してリアクション</p>
@@ -24,8 +22,7 @@
         </button>
       </div>
 
-      <div class ="bg-blue-300 container mx-auto md:w-1/2 my-20 py-3.5 text-center text-black text-3xl
-                      rounded-lg border-solid border-4 border-white shadow-outline">
+      <div class ="course-layout">
         <h3 class = "font-bold mb-4">Self Translation</h3>
         <img class="mx-auto w-1/2 h-24" src="/assets/translation.svg">
         <p class="text-lg mt-4">日本語文を直感的に英訳</p>
@@ -40,24 +37,24 @@
     <div v-else>
       <div class="text-white pt-8">
         <h1 class="mb-8 text-4xl font-bold">SelfTalkEnglish</h1>
-        <h2 class="md:text-2xl mb-8">”独り言英語学習”で実践力を磨く</h2>
+        <h2 class="home-subtitle">”独り言英語学習”で実践力を磨く</h2>
         <h3>中級者〜上級者向け英語学習アプリ</h3>
       </div>
 
       <div class="inline-block">
-        <button class= "my-10 bg-blue-500 hover:bg-blue-300 text-white font-semibold hover:text-white py-2 px-4 border border-solid hover:border-transparent rounded-full">
+        <button class= "my-10 bg-blue-500 hover:bg-blue-300 text-white font-semibold  py-2 px-4 hover:border-transparent rounded-full">
         <router-link to="/users/new">ユーザー登録</router-link></button>
 
-        <button @click="guestLogin" class= "md:ml-8 bg-green-500 hover:bg-green-200 hover:border-green-200 text-white font-semibold py-2 px-4 border border-solid rounded-full" >
+        <button @click="guestLogin" class="ml-4 bg-green-500 hover:bg-green-300 text-white font-semibold  py-2 px-4 hover:border-transparent rounded-full" >
         ゲストログイン</button>
       </div>
 
       <div class="home-div">
-        <router-link to="/about" class="md:text-2xl md:w-1/3 max-w-md mx-auto border border-solid border-white rounded-lg bg-blue-500 bg-opacity-75 p-2 hover:bg-blue-300 block">"独り言英語学習"とは</router-link>
+        <router-link to="/about" class="home-link-about">"独り言英語学習"とは</router-link>
       </div>
 
       <div class="home-div">
-        <p class="md:text-2xl mb-4">
+        <p class="home-subtitle">
           Writing形式の問題で独り言英語学習を行うためのコースを用意
         </p>
         <ul class="mx-auto max-w-md flex flex-col w-full">
@@ -68,7 +65,7 @@
       </div>
 
       <div class="home-div">
-        <p class="md:text-2xl">回答文は後で確認・添削可能</p>
+        <p class="home-subtitle">回答文は後で確認・添削可能</p>
         <img src="/assets/lp-img-1" class="border border-solid border-white mx-auto">
       </div>
 
