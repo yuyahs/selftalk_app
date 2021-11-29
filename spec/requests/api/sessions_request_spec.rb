83ac272b4c6f62a6375a15021f7a08235566f006
yuyahs <yuya.tennnis216@icloud.com>
2021-11-29 21:54:04 +0900

    refactor: sessions rspec

--- spec/requests/api/sessions_request_spec.rb
+++ spec/requests/api/sessions_request_spec.rb
@@ -2,38 +2,70 @@ require 'rails_helper'
 
 RSpec.describe "Api::Sessions", type: :request do
   let(:user){create(:user)}
+  let(:other_user){create(:user)}
 
-  describe "post/ login_path" do
-    it "不正な入力情報に対しログインに失敗する" do
-      post '/api/login', params: { session: { email: "invalid@example.com",
-      password: 'password' } }
-      expect(response).to have_http_status "400"
-    end
+  context "未ログインの場合" do
 
-    it "有効な入力情報に対しログインに成功する" do
-      post '/api/login', params: { session: { email: user.email,
-      password: 'password' } }
-      expect(response).to have_http_status "200"
-    end
-
-    it "ログイン後クッキーを保存する" do
-      log_in_as user
-      expect(cookies[:remember_token]).not_to eq nil
-    end
-
-  end
-
-
-
-  describe "delete/ logout_path" do
-    it "ログアウトに成功する" do
-      post '/api/login' ,params: { session: { email: user.email,
+    describe "POST/ create" do
+      it "不正な入力情報に対しログインに失敗し、エラー400を返す" do
+        post api_login_path, params: { session: { email: "invalid@example.com",
         password: 'password' } }
-      delete '/api/logout'
-      expect(response).to have_http_status "204"
-      expect(is_logged_in?).to be_falsy
+        expect(response).to have_http_status "400"
+      end
+
+      it "有効な入力情報に対しログインに成功して、レスポンス200を返す" do
+        post api_login_path, params: { session: { email: user.email,
+        password: 'password' } }
+        expect(response).to have_http_status "200"
+      end
+
+      it "ログイン後クッキーを保存する" do
+        log_in_as user
+        expect(cookies[:remember_token]).not_to eq nil
+      end
+    end
+
+    describe "DELETE/ destroy" do
+      it "ログアウトに失敗して、レスポンス400を返す" do
+        delete api_logout_path
+        expect(response).to have_http_status "400"
+        expect(is_logged_in?).to be_falsy
+      end
     end
   end
 
+  context "ログイン中の場合" do
 
+    before do
+      log_in_as user
+    end
+
+    describe "POST/ create" do
+      it "ログインに失敗し、エラー400を返す" do
+        post api_login_path, params: { session: { email: other_user.email,
+          password: 'password' } }
+          expect(response).to have_http_status "400"
+      end
+    end
+
+    describe "DELETE/ destroy" do
+      it "ログアウトに成功して、レスポンス200を返す" do
+        delete api_logout_path
+        expect(response).to have_http_status "200"
+        expect(is_logged_in?).to be_falsy
+      end
+    end
+  end
+
+  #not_guest_userのテスト [create]
+  context "ゲストログイン中の場合" do
+    describe "POST/ create" do
+      it "root_pathにリダイレクトする" do
+        guest_login
+        post api_login_path, params: { session: { email: other_user.email,
+          password: 'password' } }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
