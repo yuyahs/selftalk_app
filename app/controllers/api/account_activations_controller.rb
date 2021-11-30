e9adc622b767df86f9766761714d27d9f21bdabf
yuyahs <yuya.tennnis216@icloud.com>
2021-11-30 19:41:20 +0900

    refactor: account activation rspec

--- app/controllers/api/account_activations_controller.rb
+++ app/controllers/api/account_activations_controller.rb
@@ -1,4 +1,5 @@
 class Api::AccountActivationsController < ApplicationController
+  
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
