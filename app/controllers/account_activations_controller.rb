db9d0eaa817bab70167d209fce2a387505653324
yuyahs <yuya.tennnis216@icloud.com>
2021-12-07 08:07:13 +0900

    refactor: account activation controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,12 @@
 class AccountActivationsController < ApplicationController
-  # def edit
-  #   user = User.find_by(email: params[:email])
-  #   if user && !user.activated? && user.authenticated?(:activation, params[:id])
-  #     user.activate
-  #     log_in user
-  #     flash[:success] = "SelfTalkEnglishへようこそ！"
-  #     redirect_to root_path
-  #   else
-  #     flash[:danger] = "無効なリンクです。"
-  #     redirect_to root_path
-  #   end
-  # end
+  
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      redirect_to login_path
+    else
+      redirect_to root_path
+    end
+  end
 end
--- spec/requests/account_activations_request_spec.rb
+++ spec/requests/account_activations_request_spec.rb
@@ -1,1 +1,39 @@
+require 'rails_helper'
 
+RSpec.describe "AccountActivations", type: :request do
+  let(:user) { create(:user, :no_activated) }
+
+  context "トークンは有効だがメールアドレスが無効な場合" do
+
+    before do
+      get edit_account_activation_path(
+        user.activation_token,
+        email: 'wrong')
+    end
+
+    it "ログイン時にエラー401を返す" do
+      expect(response).to redirect_to root_path
+      post api_login_path, params: { session: { email: user.email,
+      password: 'password' } }
+      expect(response).to have_http_status "401"
+    end
+  end
+
+  context "有効なトークンと有効なメールアドレスが送信された場合" do
+
+   before do
+     get edit_account_activation_path(
+        user.activation_token,
+        email: user.email,
+      )
+    end
+
+    it "ログイン画面に遷移し、レスポンス200を返す" do
+      expect(response).to redirect_to login_path
+      post api_login_path, params: { session: { email: user.email,
+        password: 'password' } }
+        expect(response).to have_http_status "200"
+    end
+  end
+
+end
