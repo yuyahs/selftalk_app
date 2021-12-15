cfdab6a5b067b1546a303cc750f6c32a25329154
yuyahs <yuya.tennnis216@icloud.com>
2021-12-15 22:09:54 +0900

    fix

--- app/javascript/src/App.vue
+++ app/javascript/src/App.vue
@@ -25,7 +25,7 @@
     },
     methods : {
     createTitleDesc : function(routeInstance){
-         // タイトルを設定
+         
         if(routeInstance.meta.title){
             const setTitle = routeInstance.meta.title;
             document.title = setTitle;
