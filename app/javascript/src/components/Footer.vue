1420aa2442d309cb30b6c6ffb499160bb46ca4d9
yuyahs <yuya.tennnis216@icloud.com>
2021-12-17 19:48:54 +0900

    style: footer css

--- app/javascript/src/components/Footer.vue
+++ app/javascript/src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="bg-blue-900 text-center text-white border-t-2 border-fuchsia-600">
-    <div class="p-16">
-      <ul class="space-x-10 border-b-2 border-fuchsia-600 flex flex-row items-center justify-center">
-          <li><router-link to="/">home</router-link></li>
-          <li><router-link to="/about">about</router-link></li>
-          <li><router-link to="/contacts">conatct</router-link></li>
-          <li><router-link to="/policy">利用規約</router-link></li>
+  <footer class="footer-overall">
+    <div class="footer-div">
+      <ul class="footer-links">
+          <li class="footer-link"><router-link to="/">home</router-link></li>
+          <li class="footer-link"><router-link to="/about">about</router-link></li>
+          <li class="footer-link"><router-link to="/contacts">conatct</router-link></li>
+          <li class="footer-link"><router-link to="/policy">利用規約</router-link></li>
       </ul>
     </div>
   </footer>
