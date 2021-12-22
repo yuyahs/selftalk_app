d612a2545a9e39a2db6349200f05ea5e153a4abe
yuyahs <yuya.tennnis216@icloud.com>
2021-12-22 20:01:47 +0900

    style: transfer tailwind to custom.scss

--- app/javascript/src/pages/Error.vue
+++ app/javascript/src/pages/Error.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="text-white text-3xl text-center">
-    <h1 class="mb-8">404 NOT FOUND</h1>
-    <p class="mb-36">お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。 また、URL、ファイル名にタイプミスがないか再度ご確認ください。</p>
-    <router-link :to="{ name: 'home' }" class="underline hover:text-blue-500">
+  <div class="error-wrapper">
+    <h1 class="error-title">404 NOT FOUND</h1>
+    <p class="error-sentence">お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。また、URL,ファイル名にタイプミスがないか再度ご確認ください。</p>
+    <router-link :to="{ name: 'home' }" class="error-home-link">
       HOME画面に戻る
     </router-link>
   </div>
