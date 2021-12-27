0132d606317f10ca030b8f785ecd09dbfe612168
yuyahs <yuya.tennnis216@icloud.com>
2021-12-28 08:36:13 +0900

    fix: production.rb mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,8 @@ Rails.application.configure do
     :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => Rails.application.credentials.gmail[:user_name],
+    :password       => Rails.application.credentials.gmail[:password]
     :domain         => 'gmail.com',
     :enable_starttls_auto => true
   }
