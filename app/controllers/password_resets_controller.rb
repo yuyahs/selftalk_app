98f4691cc6d6017827b4d04126cdeb1805437e62
yuyahs <yuya.tennnis216@icloud.com>
2021-12-30 14:36:21 +0900

    refactor: delete and edit request spec files

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,8 @@
 class PasswordResetsController < ApplicationController
 
+  #validationについてはapi/pasword_resetsのupdate実行時に行われるのでここでは設けない
+  #メールに組み込まれるurlに'api'が含まれると違和感があるのでeditだけ通常にcontrollerで処理する
   def edit
   end
-  
+
 end
