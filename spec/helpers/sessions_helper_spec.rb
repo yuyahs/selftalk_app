680e0755a180350aa787f07703cc79fedc84c523
yuyahs <yuya.tennnis216@icloud.com>
2021-12-30 13:43:16 +0900

    refactor: delete spec helper files

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
   describe 'current_user' do
+    let(:user){create(:user)}
     before do
-      @user = create(:user)
-      remember @user
+      remember user
     end
 
     it 'current_user returns right when session is nil' do
-      expect(current_user).to eq @user
+      expect(current_user).to eq user
       expect(is_logged_in?).to be_truthy
     end
     it 'current_user returns nil when remember digest is wrong' do
-      @user.update_attribute(:remember_digest, User.digest(User.new_token))
+      user.update_attribute(:remember_digest, User.digest(User.new_token))
       expect(current_user).to eq nil
     end
   end
