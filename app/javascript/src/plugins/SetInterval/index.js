4ac50411a107507b0a24a5dcdd298f1c7f6183e5
yuyahs <yuya.tennnis216@icloud.com>
2022-01-02 22:35:27 +0900

    refactor: add comment on plugin settimer

--- app/javascript/src/plugins/SetInterval/index.js
+++ app/javascript/src/plugins/SetInterval/index.js
@@ -1,5 +1,6 @@
 export default {
   install (app) {
+    //Course.vueで使用するタイマー。ページ遷移時に裏でタイマーが作動し続けるのを防ぐ。
     app.config.globalProperties.$intervals = []
     app.config.globalProperties.$setInterval = (func, intervalMilliSec) => {
       const timerId = setInterval(() => {
@@ -11,7 +12,7 @@ export default {
     app.config.globalProperties.$clearInterval = (timerId) => {
       clearInterval(timerId)
       app.config.globalProperties.$intervals =[]
-   
+
     }
     app.config.globalProperties.$clearAllIntervals = () => {
       app.config.globalProperties.forEach(clearInterval)
