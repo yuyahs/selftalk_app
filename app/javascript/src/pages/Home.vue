141a92019b2ff42718742d77f57821771d5ee0d0
yuyahs <yuya.tennnis216@icloud.com>
2022-01-09 14:02:55 +0900

    style: lp sentences

--- app/javascript/src/pages/Home.vue
+++ app/javascript/src/pages/Home.vue
@@ -45,8 +45,8 @@
     <div v-else>
       <div class="home-title-wrapper">
         <h1 class="lp-title">SelfTalkEnglish</h1>
-        <h2 class="home-subtitle">「点数は取れるけど、話せない、、」を脱したい方に向ける</h2>
-        <h3 class="home-subtitle">”独り言英語学習”実践アプリ</h3>
+        <h2 class="home-subtitle">「テストの点数は取れるけど、話せない、、」</h2>
+        <h3 class="home-subtitle">から脱する”独り言英語学習”実践アプリ</h3>
       </div>
 
       <div class="inline-block">
