393aa305e90aa4fe7d6f24f17783d78ade160a2b
yuyahs <yuya.tennnis216@icloud.com>
2022-01-20 20:19:44 +0900

    doc and fix: add rubocop test to circleci config and fix things rubocop pointed out

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,12 @@ module UsersHelper
 
   def yesterday_answer
     yesterday = Time.zone.yesterday
-    yesterday_answer = current_user.answers.where(:created_at => yesterday ..  )
+    current_user.answers.where(:created_at => yesterday ..  )
   end
 
   def today_answers
     today = Time.zone.now
-    today_answers = current_user.answers.where(created_at: today.all_day)
+    current_user.answers.where(created_at: today.all_day)
   end
 
 end
