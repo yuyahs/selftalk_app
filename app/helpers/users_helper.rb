31f8f3c41c315860e7b81c310a1878c877aa6143
yuyahs <yuya.tennnis216@icloud.com>
2022-02-13 20:33:35 +0900

    refactor: add comment on api/users_controller

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 
   #学習記録が連続しているか判別するために昨日と今日のanswerを取得する
+  #api/users_controllerのshowアクションで使用
   def yesterday_answer
     yesterday = Time.zone.yesterday
     current_user.answers.where(:created_at => yesterday ..  )
